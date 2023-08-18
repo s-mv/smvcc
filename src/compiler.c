cfb754cd6782a092568adbd81a218dd789bc884a
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2023-08-18 19:33:25 +0530

    refactored

--- src/compiler.c
+++ src/compiler.c
@@ -1,17 +1,18 @@
 #include "compiler.h"
 
-CompilerStatus compile_file(const char *inpath, const char *outpath, int flags) {
+CompilerStatus compile_file(const char *inpath, const char *outpath,
+                            int flags) {
   CompileProcess *proc = compile_process_create(inpath, outpath, flags);
-  if(!proc) return COMPILER_ERROR;
+  if (!proc) return COMPILER_ERROR;
 
   /* TODO */
   // lexical analysis
-  
+
   /* TODO */
   // parsing
-  
+
   /* TODO */
-  // code generation 
+  // code generation
 
   // finally free the process
 
--- src/main.c
+++ src/main.c
@@ -7,21 +7,21 @@ int main(int argc, char **argv) {
   // temporary
   // this isn't exactly good code but eh
   if (argc < 3) {
-    printf("%s (v0.0.1): usage:\n%s <filename>.c <outname>\n", argv[0], argv[0]);
+    printf("%s (v0.0.1): usage:\n%s <filename>.c <outname>\n", argv[0],
+           argv[0]);
     return 1;
   }
   // ...otherwise
 
   CompilerStatus success = compile_file(argv[1], argv[2], 0);
 
-  if(success == COMPILER_ERROR) {
+  if (success == COMPILER_ERROR) {
     printf("errors handling code\n");
     return 1;
   }
   // ...otherwise
 
   printf("wow this works!\n");
-
 
   return 0;
 }
